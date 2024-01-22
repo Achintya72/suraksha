aa8abde96c15826457cdd38df7065341e2b6c053
Achintya Agrawal <achintya.agrawal.90@gmail.com>
2024-01-21 23:38:10 -0800

    Added Gala Promo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,11 +5,21 @@ import Button from "@/components/Button";
 import styles from "./nav.module.css";
 import classNames from "../../hooks/classnames";
 import { useScrollPosition } from "@/hooks/useScrollPosition";
+import Icon from "../Icon";
+import { useState } from "react";
 
 export default function Navbar() {
+    const [sawGala, changeGala] = useState(true);
+
     const scrollPosition = useScrollPosition();
     return (
         <nav className={classNames(styles.navbar, scrollPosition > 20 ? styles.scrolled : styles.top)}>
+            <div className={classNames(styles.galaPopup, sawGala ? null : styles.hide)}>
+                <div className={classNames(styles.galaText, "layout")}>
+                    <p className="caption">Register for Our Annual Suraksha Gala</p>
+                    <Icon size={24} name="close" onClick={() => changeGala(false)} />
+                </div>
+            </div>
             <div className={`${styles.navbarContent} layout`}>
                 <Link href="/"><h5>Suraksha</h5></Link>
                 <div className={styles.navLinks}>
