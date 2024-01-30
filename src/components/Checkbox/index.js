502ff3fe318414de4ca001c09d211742175a6183
Achintya Agrawal <achintya.agrawal.90@gmail.com>
2024-01-29 23:02:42 -0800

    Created a Super Awesome CheckBox Component

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,23 @@
+"use client";
+
+import classNames from "@/hooks/classnames";
+import Icon from "../Icon";
+import styles from "./checkbox.module.css";
+import { useState } from "react";
+
+
+export default function Checkbox({ onChange })  {
+    const [checked, changeChecked] = useState(false);
+
+    const handleClick = () => {
+        console.log("Click");
+        changeChecked(prev => !prev);
+        onChange(checked);
+    }
+    return (
+        <div className={classNames(styles.checkbox, checked  && styles.checkedRoot)} onClick={handleClick}>
+            <div className={classNames(styles.bg, checked && styles.checked)} />
+            <Icon name="check" size={12} />
+        </div>
+    )
+}
