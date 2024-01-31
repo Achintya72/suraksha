976203e2b6801a865ee0aaadb7a719e1403b6e1b
Achintya Agrawal <achintya.agrawal.90@gmail.com>
2024-01-31 01:33:57 -0800

    tweaked counter functionality and added different colors

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,31 +1,26 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./counter.module.css";
+import classNames from "@/hooks/classnames";
 
-export default function Counter({ label, limit = 10 }) {
-    const [value, changeValue] = useState(0);
-    
-    const updateCount = (val) => {
-        changeValue(prev => {
-            let newVal = prev + val;
-            if(newVal < 0) {
-                newVal = limit;
-            }
-            else if(newVal > limit) {
-                newVal = 0;
-            }
-            return newVal;
-        })
+export default function Counter({ val = 0, label, limit = 10, onChange, type = "regular" }) {
+
+    const updateCount = (v) => {
+        if (val + v >= 0 && val + v <= limit) {
+            onChange(val + v);
+        }
     }
 
     return (
         <div className={styles.counterRoot}>
-            <p className="caption">{label}</p>
+            {label != undefined &&
+                <p className="caption">{label}</p>
+            }
             <div className={styles.counter}>
-                <div className={styles.button} onClick={() => updateCount(-1)}><p className="caption">-</p></div>
-                <div className={styles.button}><p className="caption">{value}</p></div>
-                <div className={styles.button} onClick={() => updateCount(1)}><p className="caption">+</p></div>
+                <div className={classNames(styles.button, type == "regular" ? styles.regular : styles.tinted)} onClick={() => updateCount(-1)}><p className="caption">-</p></div>
+                <div className={classNames(styles.button, type == "regular" ? styles.regular : styles.tinted)}><p className="caption">{val}</p></div>
+                <div className={classNames(styles.button, type == "regular" ? styles.regular : styles.tinted)} onClick={() => updateCount(1)}><p className="caption">+</p></div>
             </div>
         </div>
     )
