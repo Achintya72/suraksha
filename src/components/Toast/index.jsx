1526601c93a3ab893a1cee6ccff3a6b04180d875
Achintya Agrawal <achintya.agrawal.90@gmail.com>
2024-01-31 01:30:56 -0800

    Moved Removal Logic to Toast

--- src/components/Toast/index.jsx
+++ src/components/Toast/index.jsx
@@ -1,15 +1,27 @@
+"use client";
+
 
 import classNames from "@/hooks/classnames";
 import styles from "./toast.module.css";
 import Icon from "../Icon";
+import { useEffect } from "react";
 
 
-export default function Toast({ type = "success", visible = false, message, toggleVisibility }) {
+export default function Toast({ error=null, changeError, visible = false, message, toggleVisibility }) {
+    useEffect(() => {
+        if(visible == true) {
+            setTimeout(() => {
+                changeError(false);
+                toggleVisibility(false);
+            }, 5000);
+        }
+    }, [visible, toggleVisibility, changeError]);
+    
     return (
         <>
         {visible &&
-            <div className={classNames(styles.toast, type == "success" ? styles.success : styles.error)}>
-                <a>{message}</a>
+            <div className={classNames(styles.toast, error ? styles.error:  styles.success)}>
+                <a>{error ? error: message}</a>
                 <Icon size={20} name="close" onClick={() => toggleVisibility(false)}/>
             </div>
         }
