771206caf504fd5b6b2084da454502dff1aa7e2d
Achintya Agrawal <achintya.agrawal.90@gmail.com>
2024-01-31 01:38:50 -0800

    Added Counter Component to Checkout Summary

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -8,13 +8,15 @@ import useDarkImage from '@/hooks/darkenImage';
 import classNames from '@/hooks/classnames';
 import { useContext, useState } from 'react';
 import StoreContext from '@/components/StoreContext';
+import Counter from '@/components/Counter';
+import useRedirectFunction from '@/hooks/useRedirectFunction';
 
 const stageClasses = [styles.left, styles.center, styles.right];
 
 export default function Checkout() {
     const darken = useDarkImage();
     const [stage, changeStage] = useState(0);
-    const { items, getItemName, getTotalCost } = useContext(StoreContext);
+    const { items, getItemName, removeItem, getTotalCost } = useContext(StoreContext);
 
     const next = () => {
         changeStage(prev => {
@@ -54,38 +56,44 @@ export default function Checkout() {
                     <div className={styles.orderSummary}>
                         <div className={styles.tabSelect}>
                             <div className={classNames(styles.currentBg, stageClasses[stage])} />
-                            <a className={stage == 0 && styles.current}>Basic Info</a>
-                            <a className={stage == 1 && styles.current}>Billing Info</a>
-                            <a className={stage == 2 && styles.current}>Completion</a>
+                            <a className={stage == 0 ? styles.current : undefined}>Basic Info</a>
+                            <a className={stage == 1 ? styles.current : undefined}>Billing Info</a>
+                            <a className={stage == 2 ? styles.current : undefined}>Completion</a>
                         </div>
                         <h6>Order Summary</h6>
-                        <div className={styles.orderItems}>
-                        {
-                            Object.keys(items).map(key => (
-                                <div key={key} className={styles.orderItem}>
-                                    <p className="caption">{getItemName(key)}</p>
-                                    <p>{items[key].quantity} x ${items[key].price}</p>
+                        {total != 0 ?
+                            <div className={styles.orderItems}>
+                                {
+                                    Object.keys(items).map(key => (
+                                        <div key={key} className={styles.orderItem}>
+                                            <p className="caption">{getItemName(key)}</p>
+                                            <Counter type="tinted" val={items[key].quantity} onChange={(v) => {
+                                                stage != 2 && removeItem(key, items[key].quantity - v, items[key].price);
+                                            }} />
+                                            <p>${Math.round(items[key].price * items[key].quantity * 100) / 100}</p>
+                                        </div>
+                                    ))
+                                }
+                                <hr style={{ width: "100%" }} />
+                                <div className={styles.orderItem}>
+                                    <p className='caption'>Subtotal</p>
+                                    <p>${Math.round(total * 100) / 100}</p>
                                 </div>
-                            ))
-                        }
-                        <hr style={{width: "100%"}}/>
-                        <div className={styles.orderItem}>
-                            <p className='caption'>Subtotal</p>
-                            <p>${Math.round(total * 100) / 100}</p>
-                        </div>
-                        <div className={styles.orderItem}>
-                            <p className="caption">Tax</p>
-                            <p>${Math.round((total / 10) * 100) / 100}</p>
-                        </div>
-                        <div className={styles.orderItem}>
-                            <p className="caption">Shipping</p>
-                            <p>$3.00</p>
-                        </div>
-                        <div className={styles.orderItem}>
-                            <p className="caption">Total</p>
-                            <p>${Math.round((total * 1.1 + 3) * 100) / 100}</p>
-                        </div>
-                        </div>
+                                <div className={styles.orderItem}>
+                                    <p className="caption">Tax</p>
+                                    <p>${Math.round((total / 10) * 100) / 100}</p>
+                                </div>
+                                <div className={styles.orderItem}>
+                                    <p className="caption">Shipping</p>
+                                    <p>$3.00</p>
+                                </div>
+                                <div className={styles.orderItem}>
+                                    <p className="caption">Total</p>
+                                    <p>${Math.round((total * 1.1 + 3) * 100) / 100}</p>
+                                </div>
+                            </div>
+                        :
+                        <p>Cart is Empty</p>}
                     </div>
                 </div>
             </section>
@@ -105,14 +113,14 @@ const BasicInfo = ({ next }) => {
     const handleChange = (e) => {
         changeInfo(prev => {
             prev[e.target.name] = e.target.value;
-            return {...prev};
+            return { ...prev };
         })
     }
 
     const handleSubmit = () => {
         let complete = true;
         Object.keys(info).forEach(key => {
-            if(info[key].length == 0) complete = false; 
+            if (info[key].length == 0) complete = false;
         });
         complete && next();
     }
@@ -121,12 +129,12 @@ const BasicInfo = ({ next }) => {
         <div className={styles.basicInfo}>
             <h2>Basic Info</h2>
             <div className={styles.nameInfo}>
-                <Input name="fname" placeholder="First Name*" value={info.fname} onChange={handleChange}/>
-                <Input name="lname" placeholder="Last Name*" value={info.lname} onChange={handleChange}/>
+                <Input name="fname" placeholder="First Name*" value={info.fname} onChange={handleChange} />
+                <Input name="lname" placeholder="Last Name*" value={info.lname} onChange={handleChange} />
             </div>
-            <Input name="email" placeholder="Email Address*" value={info.email} onChange={handleChange}/>
-            <Input name="tel" placeholder="Phone Number" value={info.tel} onChange={handleChange}/>
-            <Input name="dedication" placeholder="Dedication Message" value={info.dedication} onChange={handleChange}/>
+            <Input name="email" placeholder="Email Address*" value={info.email} onChange={handleChange} />
+            <Input name="tel" placeholder="Phone Number" value={info.tel} onChange={handleChange} />
+            <Input name="dedication" placeholder="Dedication Message" value={info.dedication} onChange={handleChange} />
             <Button onClick={handleSubmit}>Next</Button>
         </div>
     )
@@ -147,14 +155,14 @@ const BillingInfo = ({ back, next }) => {
     const handleChange = (e) => {
         changeInfo(prev => {
             prev[e.target.name] = e.target.value;
-            return {...prev};
+            return { ...prev };
         })
     }
 
     const handleSubmit = () => {
         let complete = true;
         Object.keys(info).forEach(key => {
-            if(info[key].length == 0) complete = false; 
+            if (info[key].length == 0) complete = false;
         });
         complete && next();
     }
@@ -162,16 +170,16 @@ const BillingInfo = ({ back, next }) => {
     return (
         <div className={styles.basicInfo}>
             <h2>Billing Info</h2>
-            <Input name="address" autocomplete="street-address" placeholder="Street Address" onChange={handleChange} value={info.address}/>
+            <Input name="address" autocomplete="street-address" placeholder="Street Address" onChange={handleChange} value={info.address} />
             <div className={styles.nameInfo} >
-                <Input name="city" placeholder="City" onChange={handleChange} value={info.city}/>
-                <Input name="state" placeholder="State" onChange={handleChange} value={info.state}/>
-                <Input name="zip" placeholder="Zip" onChange={handleChange} value={info.zip}/>
+                <Input name="city" placeholder="City" onChange={handleChange} value={info.city} />
+                <Input name="state" placeholder="State" onChange={handleChange} value={info.state} />
+                <Input name="zip" placeholder="Zip" onChange={handleChange} value={info.zip} />
             </div>
-            <Input name="card" placeholder="Card Number" onChange={handleChange} value={info.card}/>
+            <Input name="card" placeholder="Card Number" onChange={handleChange} value={info.card} />
             <div className={styles.nameInfo}>
-                <Input name="expiration" placeholder="Exp. Date" onChange={handleChange} value={info.expiration}/>
-                <Input name="cvv" placeholder="CVV" onChange={handleChange} value={info.cvv}/>
+                <Input name="expiration" placeholder="Exp. Date" onChange={handleChange} value={info.expiration} />
+                <Input name="cvv" placeholder="CVV" onChange={handleChange} value={info.cvv} />
             </div>
             <div className={styles.nameInfo}>
                 <Button onClick={back}>Back</Button>
@@ -182,11 +190,17 @@ const BillingInfo = ({ back, next }) => {
 }
 
 const Completion = ({ back }) => {
+    const homeRedirect = useRedirectFunction("/");
+    const {clearCart} = useContext(StoreContext);
+
     return (
         <div className={styles.basicInfo}>
             <h2>Order Placed.</h2>
             <h6>Thank you for your contribution to our cause. You will receive an email shortly with the details of your order.</h6>
-            <Button>Done</Button>
+            <Button onClick={() => {
+                clearCart();
+                homeRedirect();
+            }}>Done</Button>
         </div>
     )
 }
